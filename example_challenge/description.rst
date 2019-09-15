d72ad8727c5ae95fcd87c9b3fa018e60e5dd6ccb
tony <Ball00M0wgl1>
2019-09-15 11:10:42 +0100

    Producer script

--- example_challenge/description.rst
+++ example_challenge/description.rst
@@ -1,17 +1,17 @@
-Quora Python Beginners - Example Challenge
-==========================================
+The following is a fully set of test data for a the 'example_challenge'. The description is ::
 
-A function that takes two numbers as postional arguments and adds them together, returning the result.
+    A function that takes two numbers as postional arguments and adds them together, returning the result.
 
-The function will take two numbers (integers or floats), and return the two numbers added together, with the
-following reservations :
+    The function will take two numbers (integers or floats), and return the two numbers added together, with the
+    following reservations :
 
- - If either value is zero the result returned should be zero
- - If either value is negative the result should be zero
+     - If either value is zero the result returned should be zero
+     - If either value is negative the result should be zero
+     - If both values are negative the function should raise a ValueError exception.
 
-There is no requirement for the function to test the type of either argument.
+    There is no requirement for the function to test the type of either argument.
 
-Exceptions
-----------
+    Exceptions
+    ----------
 
-The Function is not required to raise any exceptions.
+    The Function is required to raise a ValueError exception when both arguments are negative.
---
+++ example_challenge/testdata_producer.py
@@ -0,0 +1,50 @@
+import random
+import json
+
+data_set = []
+
+# Both ints > 0
+for index in range(100):
+    a,b = random.randint(1,1001), random.randint(1,1001)
+    data_set.append({'id':f'{index:04}', 'arguments':f'{a},{b}','return':f'{a+b}'})
+
+# a is an integer, b is a float
+for index in range(100, 200):
+    a,b = random.randint(1,1001), random.uniform(1,1000)
+    data_set.append({'id':f'{index:04}', 'arguments':f'{a},{b}','return':f'{a+b}'})
+
+# b is an integer, a is a float
+for index in range(200,300):
+    b,a = random.randint(1,1001), random.uniform(1,1000)
+    data_set.append({'id':f'{index:04}', 'arguments':f'{a},{b}','return':f'{a+b}'})
+
+# a is 0, b is anything
+for index in range(300,400):
+    a,b = 0, random.randint(1,1001)
+    data_set.append({'id':f'{index:04}', 'arguments':f'{a},{b}','return':'0'})
+
+# b is 0, a is anything
+for index in range(400,500):
+    b , a = 0, random.randint(1,1001)
+    data_set.append({'id':f'{index:04}', 'arguments':f'{a},{b}','return':'0'})
+
+# both a & b are zero
+data_set.append({'id':f'600', 'arguments':f'{a},{b}','return':'0'})
+
+# a is -ve int, b is +ve int
+for index in range(700,800):
+    a,b = random.randint(-1000,0), random.uniform(-1001,1000)
+    data_set.append({'id':f'{index:04}', 'arguments':f'{a},{b}','return':f'{0}'})
+
+# a is +ve int, b is  -e int
+for index in range(800,900):
+    b,a = random.randint(-1000,0), random.uniform(-1001,1000)
+    data_set.append({'id':f'{index:04}', 'arguments':f'{a},{b}','return':f'{0}'})
+
+# a is -ve int, b is  -e int
+for index in range(800,900):
+    b,a = random.randint(-1000,0), random.uniform(-1001,0)
+    data_set.append({'id':f'{index:04}', 'arguments':f'{a},{b}','raised':'ValueError'})
+
+with open('testdata.json', 'w') as json_fp:
+    json.dump(obj=data_set, fp=json_fp)
